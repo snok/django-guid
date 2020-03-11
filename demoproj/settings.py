9c2fab12c3c783dd1965099e2c7df26496d0c01b
Sondre Lillebø Gundersen <sondre.gundersen@intility.no>
2020-03-11 11:22:17 +0100

    .

--- demoproj/settings.py
+++ demoproj/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-x = "asdasd"
+
 SECRET_KEY = 'secret'
 
 DEBUG = True
