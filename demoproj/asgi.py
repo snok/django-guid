d6db1ebaace58958b236fc7a106421386235740c
Sondre Lillebø Gundersen <sondrelg@live.no>
2022-11-23 22:02:15 +0100

    chore: Upgrade demo project
    Added anything missing from the Django v4.1 start project.

--- demoproj/asgi.py
+++ demoproj/asgi.py
@@ -1,12 +1,3 @@
-"""
-ASGI config for demoproj_asgi project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
