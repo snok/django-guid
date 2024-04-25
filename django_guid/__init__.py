4d80d58a12f9f09c0ed34d40c3418ed1a77938c6
Jonas Krüger Svensson <jonas.svensson@intility.no>
2024-04-25 17:46:33 +0200

    chore: version bump

--- django_guid/__init__.py
+++ django_guid/__init__.py
@@ -2,7 +2,7 @@ import django
 
 from django_guid.api import clear_guid, get_guid, set_guid  # noqa F401
 
-__version__ = '3.4.0'
+__version__ = '3.5.0'
 
 if django.VERSION < (3, 2):
     default_app_config = 'django_guid.apps.DjangoGuidConfig'
