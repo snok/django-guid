9b37cf52f0f2f34394bd0cd0c8a982c4301f2efa
Christian Hartung <6785871+hartungstenio@users.noreply.github.com>
2026-05-01 11:23:36 -0300

    chore: expand CI matrix and upgrade dev tooling (#158)

--- demoproj/urls.py
+++ demoproj/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import path
 
 from demoproj.views.sync_views import index_view, no_guid, rest_view
